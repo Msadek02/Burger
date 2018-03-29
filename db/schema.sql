1581aac7f7a0ae65f1ad64451073cc487bf9e9b1
Msadek02 <Thunder_axe_90@hotmail.com>
2018-03-28 22:20:59 -0400

    m

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
-CREATE TABLE burgers;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY(id)
-);
-	
+CREATE TABLE burgers(
+id int AUTO_INCREMENT,
+PRIMARY KEY (id),
+burger_name varchar(250) NOT NULL,
+devoured BOOLEAN default false,
+date TIMESTAMP);
