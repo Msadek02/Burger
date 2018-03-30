866b6548612bcb353dcc369761aaef6b6fd1bb64
Msadek02 <Thunder_axe_90@hotmail.com>
2018-03-30 15:04:22 -0400

    m

--- config/connection.js
+++ config/connection.js
@@ -11,9 +11,19 @@ else{
     host     : 'localhost',
     user     : 'root',
     password : 'root', // Add your password
-    database : 'burgers_db' // Add your database
+    database : 'burgers_db', // Add your database
+    port: 3306
   });
 }
+
+// Make the connection to MySQL
+connection.connect(function(err) {
+  if (err) {
+    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+    return;
+  }
+  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+});
 
 
 // Export the Connection
