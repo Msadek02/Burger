3bb82b1c0a10ec8f777ddc0480098e5371c53a20
Msadek02 <Thunder_axe_90@hotmail.com>
2018-03-30 14:28:58 -0400

    m

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
-var express = require("express");
-var bodyParser = require("body-parser");
 
-var PORT = process.env.PORT || 8080;
+// Node Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
 
 var app = express();
+//Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+// app.use(express.static('public'));
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// Handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-// parse application/json
-app.use(bodyParser.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+var router = require('./controllers/burgers_controllers.js');
+app.use('/', router);
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controllers.js");
-
-app.use(routes);
-
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
-});
+// Open Server
+var port = process.env.PORT || 3000;
+app.listen(port);
